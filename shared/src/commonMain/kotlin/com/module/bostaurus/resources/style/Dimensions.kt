9d64f7db3a89a82ca0af1c9a760131f3e5451bbf
Jure Lokovsek <lokovsek.jure@gmail.com>
2025-11-03 14:53:39 +0100

    Added Dimensions.

---
+++ shared/src/commonMain/kotlin/com/module/bostaurus/resources/style/Dimensions.kt
@@ -0,0 +1,38 @@
+package com.module.bostaurus.resources.style
+
+import androidx.compose.ui.unit.dp
+
+// https://www.youtube.com/watch?v=HrBKDmmKkaU&ab_channel=Gradle
+
+object Padding {
+    val tiny = 2.dp
+    val xSmall = 4.dp
+    val small = 8.dp
+    val normal = 16.dp
+    val large = 24.dp
+    val xLarge = 32.dp
+    val xxLarge = 48.dp
+    val xxxLarge = 64.dp
+}
+
+object Size {
+    val tiny = 2.dp
+    val xSmall = 4.dp
+    val small = 8.dp
+    val normal = 16.dp
+    val large = 24.dp
+    val xLarge = 32.dp
+    val xxLarge = 48.dp
+    val xxxLarge = 64.dp
+}
+
+object Height {
+    val tiny = 2.dp
+    val xSmall = 4.dp
+    val small = 8.dp
+    val normal = 16.dp
+    val large = 24.dp
+    val xLarge = 32.dp
+    val xxLarge = 48.dp
+    val xxxLarge = 64.dp
+}
